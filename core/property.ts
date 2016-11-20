5c9aa3d376f126ac30258ca5bdcef4a166b6e77c
Marcin Mazurek <marcin@mazurek.pro>
2016-11-20 10:53:52 +0100

    Clean up

--- core/property.ts
+++ core/property.ts
@@ -1,7 +1,9 @@
-export interface Property {
+interface Property {
   readonly title: string;
   readonly location: string;
   readonly additionDate: string;
   readonly price: number;
   readonly thumbnailUrl: string;
 }
+
+export default Property;
