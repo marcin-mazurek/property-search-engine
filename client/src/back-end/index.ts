bba39c6cd19eb34f06cd5f8ec41aef4cbf242225
Marcin Mazurek <marcin@mazurek.pro>
2016-11-28 17:10:01 +0100

    Serve component templates

--- client/src/back-end/index.ts
+++ client/src/back-end/index.ts
@@ -4,9 +4,13 @@ import { Response } from 'express';
 const app = express();
 
 app.use(express.static('public'));
+app.use('/compiled', express.static('src/front-end'));
 app.use('/node_modules', express.static('node_modules'));
-app.use(
+app.get('/',
   (request: any, response: Response) => response.sendFile('public/index.html', { root: '.' })
+);
+app.use(
+  (request: any, response: Response) => response.status(404).send()
 );
 
 app.listen(3000, () => {
