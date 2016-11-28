e0ac8c1f29d2c1d9a7953c6eaf57231b30b1e4fa
Marcin Mazurek <marcin@mazurek.pro>
2016-11-28 20:20:24 +0100

    Filters cleanup

--- client/src/front-end/filters/filters.component.ts
+++ client/src/front-end/filters/filters.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Filters, { Category, Type, Market } from '../../../../core/src/filters';
 
 @Component({
   moduleId: module.id,
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: 'filters.component.html',
   styleUrls: ['filters.css']
 })
-export default class FiltersComponent { }
+export default class FiltersComponent {
+  model: Filters;
+
+  categoryOptions: [
+    { label: 'Sale', value: Category.Sale },
+    { label: 'Rental', value: Category.Rental },
+    { label: 'Exchange', value: Category.Exchange }
+  ];
+
+  typeOptions: [
+    { label: 'Apartment', value: Type.Apartment },
+    { label: 'House', value: Type.House },
+    { label: 'Room', value: Type.Room }
+  ];
+  
+  marketOptions: [
+    { label: 'Any', value: null },
+    { label: 'Primary', value: Market.Primary },
+    { label: 'Secondary', value: Market.Secondary }
+  ];
+}
