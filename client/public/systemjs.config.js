e1e2c9f0d8c04874fa7cfcf9791eb158729fef37
Marcin Mazurek <marcin@mazurek.pro>
2016-11-28 17:09:00 +0100

    Introduce material design library and SCSS compiler

--- client/public/systemjs.config.js
+++ client/public/systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'js',
+      app: 'compiled',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -23,9 +23,12 @@
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/router/upgrade': 'npm:@angular/router/bundles/router-upgrade.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/material': 'npm:@angular/material/material.umd.js',
+      
 
       // other libraries
-      'rxjs':                      'npm:rxjs'
+      'rxjs':                      'npm:rxjs',
+
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
