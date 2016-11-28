5dc135b45663fc120671f7430abffe95063486b4
Marcin Mazurek <marcin@mazurek.pro>
2016-11-28 19:52:19 +0100

    Finish off filters and refactor

--- client/src/front-end/filters/filters.component.ts
+++ client/src/front-end/filters/filters.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-filters',
-  template: `
-    <div class="filters"></div>
-  `
+  templateUrl: 'filters.component.html',
+  styleUrls: ['filters.css']
 })
 export default class FiltersComponent { }
