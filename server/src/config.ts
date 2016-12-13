92ffe2e15a64ade295da1c6e30daf018a542fecc
Marcin Mazurek <marcin@mazurek.pro>
2016-12-13 19:45:08 +0100

    Increase search result limit

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,7 +2,7 @@ import fetchOlxListing from './portals/olx/fetch-listing';
 import fetchOtodomListing from './portals/otodom/fetch-listing';
 
 export const resultsPerPage = 25;
-export const resultsLimit = 200;
+export const resultsLimit = 1000;
 
 export const fetchFunctions = [
   fetchOlxListing,
