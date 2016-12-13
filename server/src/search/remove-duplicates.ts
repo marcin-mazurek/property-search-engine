b348dafa8d4e1e30725a0a521ae43522f9817f6d
Marcin Mazurek <marcin@mazurek.pro>
2016-12-13 21:28:20 +0100

    Remove offer duplicates by combination of title and price

--- server/src/search/remove-duplicates.ts
+++ server/src/search/remove-duplicates.ts
@@ -2,5 +2,10 @@ import { uniqBy } from 'lodash';
 import Property from '../../../core/src/property';
 
 export default function removeDuplicates(properties: Property[]): Property[] {
-  return uniqBy(properties, property => property.offerUrl);
+  return removeDuplicatedUrls(
+    removeDuplicatedTitlePriceCombination(properties)
+  );
 }
+
+const removeDuplicatedUrls = (properties: Property[]) => uniqBy(properties, property => property.offerUrl);
+const removeDuplicatedTitlePriceCombination = (properties: Property[]) => uniqBy(properties, property => property.title + property.price);
