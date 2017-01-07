5e0646407d987c707de05de369c825870816d91c
Marcin Mazurek <marcin@mazurek.pro>
2017-01-07 20:49:33 +0100

    Add Gumtree listing fetcher to configuration

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,10 +1,13 @@
 import fetchOlxListing from './portals/olx/fetch-listing';
 import fetchOtodomListing from './portals/otodom/fetch-listing';
+import fetchGumtreeListing from './portals/gumtree/fetch-listing';
+import FetchFunction from '../../core/src/fetch-function';
 
 export const resultsPerPage = 25;
 export const resultsLimit = 1000;
 
 export const fetchFunctions = [
+  fetchGumtreeListing,
   fetchOlxListing,
   fetchOtodomListing
 ];
