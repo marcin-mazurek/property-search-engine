a45dcdf1c855d74b9b64aeb097d7bdede22ace9f
Marcin Mazurek <marcin@mazurek.pro>
2017-01-07 20:48:58 +0100

    Add non-breaking space handling to price parser

--- server/src/portals/shared/parse-listing-price.ts
+++ server/src/portals/shared/parse-listing-price.ts
@@ -1,4 +1,7 @@
 export default function parseListingPrice(price: string): number {
-  const unformattedPrice = price.replace('zł', '').split(' ').join('');
+  const unformattedPrice = price
+    .replace('zł', '')
+    .replace(new RegExp(' ', 'g'), '')
+    .replace(new RegExp("\u00A0", 'g'), '');
   return parseInt(unformattedPrice);
 }
