467376aeb5b6777fd9740db2754957d44c17c467
Marcin Mazurek <marcin@mazurek.pro>
2017-01-07 16:35:46 +0100

    URL builder for Gumtree - query string - WIP

---
+++ server/src/portals/gumtree/build-query-string.ts
@@ -0,0 +1,22 @@
+import Filters from '../../../../core/src/filters';
+
+export default function buildQueryString(filters: Filters): Map<string, string> {
+  const queryParams = new Map<string, string>();
+
+  if (filters.priceFrom || filters.priceTo) {
+    const value = (filters.priceFrom * 1000 || '') + ',' + (filters.priceTo * 1000 || '');
+    queryParams.set('pr', value);
+  }
+
+  if (filters.market) {
+    throw new Error('Unsupported parameter - market');
+  }
+  if (filters.areaFrom) {
+    throw new Error('Unsupported parameter - area from');
+  }
+  if (filters.areaTo) {
+    throw new Error('Unsupported parameter - area to');
+  }
+
+  return queryParams;
+}
---
+++ server/tests/portals/gumtree/build-query-string.spec.ts
@@ -0,0 +1,62 @@
+import 'mocha';
+import Filters, { Type, Category, Market } from '../../../../core/src/filters';
+import { expect } from 'chai';
+import buildQueryString from '../../../src/portals/gumtree/build-query-string';
+
+const defaultFilters = {
+  type: Type.House,
+  category: Category.Sale,
+  location: 'Kraków'
+};
+
+function mergeFilterOptionsWithDefaults(options) {
+  return Object.assign({}, defaultFilters, options);
+}
+
+describe('buildQueryString()', () => {
+  it('builds a query string with a "price from" parameter if specified', () => {
+    const qs = buildQueryString(
+      mergeFilterOptionsWithDefaults({ priceFrom: 80 })
+    );
+    expect(qs.get('pr')).to.equal('80000,');
+  });
+  
+  it('builds a query string with a "price to" parameter if specified', () => {
+    const qs = buildQueryString(
+      mergeFilterOptionsWithDefaults({ priceTo: 150 })
+    );
+    expect(qs.get('pr')).to.equal(',150000');
+  });
+  
+  it('builds a query string with a "price from" and "price to" parameters if specified', () => {
+    const qs = buildQueryString(
+      mergeFilterOptionsWithDefaults({ priceFrom: 90, priceTo: 140 })
+    );
+    expect(qs.get('pr')).to.equal('90000,140000');
+  });
+
+  it('does not include price parameter if not specified', () => {
+    const qs = buildQueryString(
+      mergeFilterOptionsWithDefaults({ priceFrom: undefined, priceTo: undefined })
+    );
+    expect(qs.has('pr')).to.be.false;
+  });
+
+  it('throws if "market" parameter is set', () => {
+    expect(() => buildQueryString(
+      mergeFilterOptionsWithDefaults({ market: Market.Secondary })
+    )).to.throw('Unsupported parameter - market');
+  });
+
+  it('throws if "area from" parameter is set', () => {
+    expect(() => buildQueryString(
+      mergeFilterOptionsWithDefaults({ areaFrom: 20 })
+    )).to.throw('Unsupported parameter - area from');
+  });
+
+  it('throws if "area to" parameter is set', () => {
+    expect(() => buildQueryString(
+      mergeFilterOptionsWithDefaults({ areaTo: 50 })
+    )).to.throw('Unsupported parameter - area to');
+  });
+});
