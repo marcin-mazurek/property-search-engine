e1cc94f2cb5928a78f93d5f13ac73ce7ab5a4912
Marcin Mazurek <marcin@mazurek.pro>
2017-01-07 20:48:28 +0100

    Add type casting to Gumtree's URL builder

--- server/src/portals/gumtree/build-url.ts
+++ server/src/portals/gumtree/build-url.ts
@@ -9,6 +9,9 @@ const basePath = 'https://www.gumtree.pl';
 export default async function buildUrl(filters: Filters, page: number) : Promise<string> {
   const url = new URLBuilder(basePath);
 
+  filters.type = Number(filters.type);
+  filters.category = Number(filters.category);
+
   if (filters.type === Type.Apartment || filters.type === Type.House) {
     url.addSegment('s-mieszkania-i-domy-sprzedam-i-kupie');
   } else if (filters.type === Type.Room) {
